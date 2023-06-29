15ef55001f3f338d93d86c286fc0a9aaf62d86fc
Tisca Catalin <indianucode0303@gmail.com>
2023-06-29 23:13:58 +0300

    Spring project initialized

---
+++ src/main/java/com/straightouttasteagu/schoolhub/SchoolhubApplication.java
@@ -0,0 +1,13 @@
+package com.straightouttasteagu.schoolhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/straightouttasteagu/schoolhub/SchoolhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.straightouttasteagu.schoolhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
