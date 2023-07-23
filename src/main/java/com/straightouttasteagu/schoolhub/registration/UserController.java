486dfbad51f3fc2a7c8adc39c14ffbecfc5d79c4
Raul Ababei <ababeiraul13@gmail.com>
2023-07-23 23:05:33 +0300

    hopefully good app user

---
+++ src/main/java/com/straightouttasteagu/schoolhub/registration/UserController.java
@@ -0,0 +1,10 @@
+package com.straightouttasteagu.schoolhub.registration;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/registration")
+public class UserController {
+
+}
