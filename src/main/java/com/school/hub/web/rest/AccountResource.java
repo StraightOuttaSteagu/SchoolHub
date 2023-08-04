aba22a5a799e0b58f31ceece471beed88e5acc28
Tisca Catalin <indianucode0303@gmail.com>
2023-08-04 19:13:19 +0300

    disabled mail verification

--- src/main/java/com/school/hub/web/rest/AccountResource.java
+++ src/main/java/com/school/hub/web/rest/AccountResource.java
@@ -7,16 +7,18 @@ import com.school.hub.service.MailService;
 import com.school.hub.service.UserService;
 import com.school.hub.service.dto.AdminUserDTO;
 import com.school.hub.service.dto.PasswordChangeDTO;
-import com.school.hub.web.rest.errors.*;
-import com.school.hub.web.rest.vm.KeyAndPasswordVM;
+import com.school.hub.web.rest.errors.EmailAlreadyUsedException;
+import com.school.hub.web.rest.errors.InvalidPasswordException;
+import com.school.hub.web.rest.errors.LoginAlreadyUsedException;
 import com.school.hub.web.rest.vm.ManagedUserVM;
 import jakarta.validation.Valid;
-import java.util.*;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 /**
  * REST controller for managing the current user's account.
@@ -64,19 +66,19 @@ public class AccountResource {
         mailService.sendActivationEmail(user);
     }
 
-    /**
-     * {@code GET  /activate} : activate the registered user.
-     *
-     * @param key the activation key.
-     * @throws RuntimeException {@code 500 (Internal Server Error)} if the user couldn't be activated.
-     */
-    @GetMapping("/activate")
-    public void activateAccount(@RequestParam(value = "key") String key) {
-        Optional<User> user = userService.activateRegistration(key);
-        if (!user.isPresent()) {
-            throw new AccountResourceException("No user was found for this activation key");
-        }
-    }
+//    /**
+//     * {@code GET  /activate} : activate the registered user.
+//     *
+//     * @param key the activation key.
+//     * @throws RuntimeException {@code 500 (Internal Server Error)} if the user couldn't be activated.
+//     */
+//    @GetMapping("/activate")
+//    public void activateAccount(@RequestParam(value = "key") String key) {
+//        Optional<User> user = userService.activateRegistration(key);
+//        if (!user.isPresent()) {
+//            throw new AccountResourceException("No user was found for this activation key");
+//        }
+//    }
 
     /**
      * {@code GET  /account} : get the current user.
@@ -109,7 +111,7 @@ public class AccountResource {
             throw new EmailAlreadyUsedException();
         }
         Optional<User> user = userRepository.findOneByLogin(userLogin);
-        if (!user.isPresent()) {
+        if (user.isEmpty()) {
             throw new AccountResourceException("User could not be found");
         }
         userService.updateUser(
@@ -138,38 +140,38 @@ public class AccountResource {
     /**
      * {@code POST   /account/reset-password/init} : Send an email to reset the password of the user.
      *
-     * @param mail the mail of the user.
+//     * @param mail the mail of the user.
      */
-    @PostMapping(path = "/account/reset-password/init")
-    public void requestPasswordReset(@RequestBody String mail) {
-        Optional<User> user = userService.requestPasswordReset(mail);
-        if (user.isPresent()) {
-            mailService.sendPasswordResetMail(user.orElseThrow());
-        } else {
-            // Pretend the request has been successful to prevent checking which emails really exist
-            // but log that an invalid attempt has been made
-            log.warn("Password reset requested for non existing mail");
-        }
-    }
-
-    /**
-     * {@code POST   /account/reset-password/finish} : Finish to reset the password of the user.
-     *
-     * @param keyAndPassword the generated key and the new password.
-     * @throws InvalidPasswordException {@code 400 (Bad Request)} if the password is incorrect.
-     * @throws RuntimeException {@code 500 (Internal Server Error)} if the password could not be reset.
-     */
-    @PostMapping(path = "/account/reset-password/finish")
-    public void finishPasswordReset(@RequestBody KeyAndPasswordVM keyAndPassword) {
-        if (isPasswordLengthInvalid(keyAndPassword.getNewPassword())) {
-            throw new InvalidPasswordException();
-        }
-        Optional<User> user = userService.completePasswordReset(keyAndPassword.getNewPassword(), keyAndPassword.getKey());
-
-        if (!user.isPresent()) {
-            throw new AccountResourceException("No user was found for this reset key");
-        }
-    }
+//    @PostMapping(path = "/account/reset-password/init")
+//    public void requestPasswordReset(@RequestBody String mail) {
+//        Optional<User> user = userService.requestPasswordReset(mail);
+//        if (user.isPresent()) {
+//            mailService.sendPasswordResetMail(user.orElseThrow());
+//        } else {
+//            // Pretend the request has been successful to prevent checking which emails really exist
+//            // but log that an invalid attempt has been made
+//            log.warn("Password reset requested for non existing mail");
+//        }
+//    }
+//
+//    /**
+//     * {@code POST   /account/reset-password/finish} : Finish to reset the password of the user.
+//     *
+//     * @param keyAndPassword the generated key and the new password.
+//     * @throws InvalidPasswordException {@code 400 (Bad Request)} if the password is incorrect.
+//     * @throws RuntimeException {@code 500 (Internal Server Error)} if the password could not be reset.
+//     */
+//    @PostMapping(path = "/account/reset-password/finish")
+//    public void finishPasswordReset(@RequestBody KeyAndPasswordVM keyAndPassword) {
+//        if (isPasswordLengthInvalid(keyAndPassword.getNewPassword())) {
+//            throw new InvalidPasswordException();
+//        }
+//        Optional<User> user = userService.completePasswordReset(keyAndPassword.getNewPassword(), keyAndPassword.getKey());
+//
+//        if (!user.isPresent()) {
+//            throw new AccountResourceException("No user was found for this reset key");
+//        }
+//    }
 
     private static boolean isPasswordLengthInvalid(String password) {
         return (
