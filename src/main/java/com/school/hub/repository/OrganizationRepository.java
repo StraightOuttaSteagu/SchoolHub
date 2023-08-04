7fcddf084d86543f7dd7fa6e2b042d06a89e70c3
Raul Ababei <ababeiraul13@gmail.com>
2023-08-04 22:24:01 +0300

    organizaition entity + repository

---
+++ src/main/java/com/school/hub/repository/OrganizationRepository.java
@@ -0,0 +1,13 @@
+package com.school.hub.repository;
+
+import com.school.hub.domain.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface OrganizationRepository extends CrudRepository<Organization, Long> {
+}
