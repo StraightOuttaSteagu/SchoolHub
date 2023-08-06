b899957d25c43eca0e522bfc369cb913b85b150b
Karo8870 <cazacuchristian@gmail.com>
2023-08-06 14:46:42 +0300

    testing staging

--- src/webapp/src/app/features/login/components/login-formular/login-formular.component.ts
+++ src/webapp/src/app/features/login/components/login-formular/login-formular.component.ts
@@ -14,7 +14,7 @@ import { AuthService } from './auth.service';
     FormAnimationFade,
     FormAnimationMargin
   ],
-})
+}) 
 export class LoginFormularComponent implements OnInit {
   authForm!: FormGroup;
 
