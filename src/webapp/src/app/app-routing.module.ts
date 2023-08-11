66d886eb2d328c4877750c7edbc00b515d5aabff
Adela Florea <adeflorea1234@gmail.com>
2023-08-11 14:24:04 +0300

    settings initialize (work in progress)

--- src/webapp/src/app/app-routing.module.ts
+++ src/webapp/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+import { SettingsModule } from './features/settings/settings.module';
 
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'settings', loadChildren: () => import('./features/settings/settings.module').then(m => m.SettingsModule) },
   { path: '', loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule) },
   { path: '**', component: PageNotFoundComponent}
 ];
