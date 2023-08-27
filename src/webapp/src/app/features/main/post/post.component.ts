c0c21be2345f597c1fb7f25d4a10201a2b4602f2
Karo8870 <cazacuchristian@gmail.com>
2023-08-27 23:58:17 +0300

    made body for post and made theme work for it

--- src/webapp/src/app/features/main/post/post.component.ts
+++ src/webapp/src/app/features/main/post/post.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThemeService } from 'src/app/core/services/theme.service';
 
 @Component({
   selector: 'app-post',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent {
+  data = {
+    assignment: true,
+    classID: "690"
+  }
 
+  constructor (private _theme: ThemeService) { }
+
+  ngOnInit(): void {
+    this._theme.setClassThemeID(this.data.classID);
+  }
 }
