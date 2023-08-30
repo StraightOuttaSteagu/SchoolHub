562fa2cd27ec10f175c06aa49288710b340b9dc9
Alex-Simedrea <simedrea.alex@gmail.com>
2023-08-30 18:50:22 +0300

    made sidebar o swipe gesture enabled only on the dashboard

--- src/webapp/src/app/features/main/settings/settings.component.ts
+++ src/webapp/src/app/features/main/settings/settings.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MenuController, ViewDidEnter } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
   templateUrl: 'settings.component.html',
   styleUrls: ['settings.component.scss']
 })
-export class SettingsComponent {
+export class SettingsComponent implements ViewDidEnter {
   editable: boolean = false;
   profileData = {
     username: 'irena',
@@ -14,5 +15,9 @@ export class SettingsComponent {
     email: 'irena@olteanu.ro',
   };
 
-  
+  constructor(private _menu: MenuController) { }
+
+  ionViewDidEnter(): void {
+    this._menu.swipeGesture(false);
+  }
 }
