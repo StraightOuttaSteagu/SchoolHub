8a2e68612cc7b2538d060e6bc753d431a4daca1c
Karo8870 <cazacuchristian@gmail.com>
2023-09-08 21:39:24 +0300

    made account connection via state management

---
+++ src/webapp/src/app/core/state-management/account/account.actions.ts
@@ -0,0 +1,10 @@
+import { accountModel } from "../models";
+
+export class GetAccount {
+    static readonly type = '[Account] Get';
+}
+
+export class UpdateAccount {
+    static readonly type = '[Account] Update';
+    constructor (public payload: accountModel) { }
+}
--- src/webapp/src/app/features/main/settings/settings.component.ts
+++ src/webapp/src/app/features/main/settings/settings.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MenuController, ViewDidEnter } from '@ionic/angular';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { UpdateAccount } from 'src/app/core/state-management/account/account.actions';
+import { AccountState } from 'src/app/core/state-management/account/account.state';
 
 @Component({
   selector: 'app-settings',
@@ -7,17 +12,47 @@ import { MenuController, ViewDidEnter } from '@ionic/angular';
   styleUrls: ['settings.component.scss']
 })
 export class SettingsComponent implements ViewDidEnter {
-  editable: boolean = false;
-  profileData = {
-    username: 'irena',
-    name: 'Irena',
-    surname: 'Olteanu',
-    email: 'irena@olteanu.ro',
-  };
 
-  constructor(private _menu: MenuController) { }
+  @Select(AccountState.selectAccount) account$!: Observable<any>;
+
+  editable: boolean = false;
+
+  accountForm: FormGroup = this._fb.group({
+    firstName: [{value: '', disabled: true}, [Validators.required]],
+    lastName: [{value: '', disabled: true}, [Validators.required]],
+    email: [{value: '', disabled: true}, [Validators.required]]
+  })
+
+  constructor(private _menu: MenuController, private _fb: FormBuilder, private _store: Store) { }
+
+  ngOnInit() {
+    this.account$.subscribe({
+      next: (account) => {
+        this.accountForm.setValue({
+          firstName: account.firstName,
+          lastName: account.lastName,
+          email: account.email
+        });
+      }
+    })
+  }
 
   ionViewDidEnter(): void {
     this._menu.swipeGesture(false);
+  }
+
+  submit(): void {
+    this.editable = !this.editable;
+    if (this.editable) {
+      this.accountForm.controls['firstName'].enable();
+      this.accountForm.controls['lastName'].enable();
+      this.accountForm.controls['email'].enable();
+    } else {
+      this._store.dispatch(new UpdateAccount(this.accountForm.getRawValue()));
+
+      this.accountForm.controls['firstName'].disable();
+      this.accountForm.controls['lastName'].disable();
+      this.accountForm.controls['email'].disable();
+    }
   }
 }
