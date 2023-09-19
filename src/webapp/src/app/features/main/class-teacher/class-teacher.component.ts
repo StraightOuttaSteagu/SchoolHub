7ed6f327c062c365437fabd2b9523051ef87c9b5
Alex-Simedrea <simedrea.alex@gmail.com>
2023-09-19 18:51:09 +0300

    made class settings for teachers

--- src/webapp/src/app/features/main/class-teacher/class-teacher.component.ts
+++ src/webapp/src/app/features/main/class-teacher/class-teacher.component.ts
@@ -3,14 +3,19 @@ import { ThemeService } from '../../../core/services/theme.service';
 import { MenuController, ViewDidEnter, ViewWillEnter, ViewWillLeave } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { icons } from '../../../shared/icons';
+import { colors } from '../../../shared/colors';
 
 @Component({
   selector: 'app-class-teacher',
   templateUrl: './class-teacher.component.html',
-  styleUrls: ['./class-teacher.component.scss']
+  styleUrls: ['./class-teacher.component.scss'],
 })
 export class ClassTeacherComponent implements ViewWillEnter, ViewWillLeave, ViewDidEnter {
-// The HTML for the classes will remain the same and the route will act as a filter that selects only some of the fields
+  // The HTML for the classes will remain the same and the route will act as a filter that selects only some of the fields
+
+  icons: any = icons;
+  colors: any = colors;
 
   data: any[] = [
     {
@@ -24,17 +29,18 @@ export class ClassTeacherComponent implements ViewWillEnter, ViewWillLeave, View
       due_date: '11.04.2023',
       type: 'assignments',
       attachments_num: 3,
-      comments_num: 1
+      comments_num: 1,
     },
     {
       title: 'Realizare compoziție',
-      content: 'Realizați o compoziție plastică în care să redați atmosfera sărbătorilor de iarnă, și în care să aveți ca element principal un felinar.',
+      content:
+        'Realizați o compoziție plastică în care să redați atmosfera sărbătorilor de iarnă, și în care să aveți ca element principal un felinar.',
       subject: 'Educație plastică',
       date: '01.10.2023',
       icon: 'pencils',
       due_date: '6.10.2023',
       type: 'assignments',
-      attachments_num: 2
+      attachments_num: 2,
     },
     {
       title: 'CARIOTIPUL UMAN PATOLOGIC',
@@ -47,16 +53,29 @@ export class ClassTeacherComponent implements ViewWillEnter, ViewWillLeave, View
       due_date: '11.04.2023',
       type: 'announcements',
       attachments_num: 3,
-      comments_num: 1
+      comments_num: 1,
     },
-    {date: '6.09.2023', excused: false, type: 'attendance'},
-    {grade: '10', date: '6.09.2023', type: 'grades'},
-    {date: '6.09.2023', excused: false, type: 'attendance'},
+    { date: '6.09.2023', excused: false, type: 'attendance' },
+    { grade: '10', date: '6.09.2023', type: 'grades' },
+    { date: '6.09.2023', excused: false, type: 'attendance' },
   ];
+  deleteAlertButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+    },
+  ];
+  selectedColor: number = 0;
+  selectedIcon: number = 0;
+  protected readonly document = document;
+  protected readonly Object = Object;
   private _routerSubscription!: Subscription;
 
-  constructor(private _route: ActivatedRoute, private _theme: ThemeService, private _menu: MenuController) {
-  }
+  constructor(private _route: ActivatedRoute, private _theme: ThemeService, private _menu: MenuController) {}
 
   ionViewWillEnter(): void {
     this._route.paramMap.subscribe(params => {
